b93da0e9877960cad7ed3b853ab262e4c93b4c5c
Shrutika-Patil1208 <shrutikapatil215@gmail.com>
2026-01-27 21:45:46 +0530

    19th commit

---
+++ basics/_19_string_tokens.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+public class _19_string_tokens {
+
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        scan.close();
+                s = s.trim();
+
+        // If string is empty
+        if (s.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        // Split using non-alphabet characters
+        String[] tokens = s.split("[^A-Za-z]+");
+
+        // Print number of tokens
+        System.out.println(tokens.length);
+
+        // Print each token
+        for (String token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+}
+
+
+
