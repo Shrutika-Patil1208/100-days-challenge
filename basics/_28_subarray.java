49d9bcb886bc4d0cde1f5e03b650730cfd21c5b7
Shrutika-Patil1208 <shrutikapatil215@gmail.com>
2026-02-05 08:29:04 +0530

    28th commit

---
+++ basics/_28_subarray.java
@@ -0,0 +1,37 @@
+// import java.io.*;
+import java.util.*;
+// import java.text.*;
+// import java.util.regex.*;
+// import java.math.*;
+
+public class _28_subarray {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+                Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        // Check all subarrays
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum < 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+        sc.close();
+    }
+}
+
